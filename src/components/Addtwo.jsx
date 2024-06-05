4fa4658f3cb087f88421b82a475bfa8845a4b54b
Shakhawat Bijoy <piyash2003b@gmail.com>
2024-06-05 19:48:55 +0600

    ok

--- src/components/Addtwo.jsx
+++ src/components/Addtwo.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+
 import Image from './layer/Image'
 import ad2 from '../assets/ad2.jpg'
 
 const Addtwo = () => {
   return (
-    <div className='lg:mt-32 mt-8'>
-        <Image src={ad2}/>
+    <div className='lg:mt-32 mt-8 w-full'>
+        <Image className="w-full" src={ad2}/>
     </div>
   )
 }
